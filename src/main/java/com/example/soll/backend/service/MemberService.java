8dbb23c9f172d92ad14a48d3cb9d5875163179e6
ComNewbie <iforking3@naver.com>
2024-05-26 15:29:29 +0900

    feat: 이메일 중복 검사 구현
    회원가입시, 이메일 중복검사하는 기능을 구현했습니다

--- src/main/java/com/example/soll/backend/service/MemberService.java
+++ src/main/java/com/example/soll/backend/service/MemberService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -25,5 +26,10 @@ public class MemberService {
         }else{
             throw new UserNotFoundException(ErrorCode.USER_NOT_FOUND_EXCEPTION);
         }
+    }
+
+    @Transactional(readOnly = true)
+    public boolean validation(String email) {
+        return memberRepository.existsByEmail(email);
     }
 }
